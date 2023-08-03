dadf4e9708668468dbd48d9de378fd6d6ce39518
Abhay 076 <yadavabhaycs@gmail.com>
2023-08-03 08:59:26 +0530

    setup Quick Foods Web App

--- src/App.js
+++ src/App.js
@@ -1,25 +1,68 @@
-import logo from './logo.svg';
-import './App.css';
-
-function App() {
+import logo from "./images/Quick Food (6).png";
+const Header = () => {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="header">
+      <div className="logo-container">
+        <img className="logo" src={logo} alt="logo" />
+      </div>
+      <div className="nav-items">
+        <ul>
+          <li>Home</li>
+          <li>About Us</li>
+          <li>Contact Us</li>
+          <li>Cart</li>
+        </ul>
+      </div>
+    </div>
+  );
+};
+
+const RestaurantCard = () => {
+  return (
+    <div className="card" style={{ background: "white" }}>
+      <img
+        className="res-logo"
+        alt="res-logo"
+        src="https://res.cloudinary.com/swiggy/image/upload/fl_lossy,f_auto,q_auto,w_660/fundrz2tguru4dkbl8wn"
+      />
+      <h3>Tiffen Depot</h3>
+      <p>South Indian,Nanakramguda</p>
+      <p>4.3 ratings</p>
+      <p>20 minuts</p>
     </div>
   );
 }
+
+const Body = () => {
+  return (
+    <div className="body">
+      <div className="search">Search</div>
+      <div className="restaurant-list">
+        <RestaurantCard />
+        <RestaurantCard />
+        <RestaurantCard />
+        <RestaurantCard />
+        <RestaurantCard />
+        <RestaurantCard />
+        <RestaurantCard />
+        <RestaurantCard />
+        <RestaurantCard />
+        <RestaurantCard />
+        <RestaurantCard />
+        <RestaurantCard />
+        <RestaurantCard />
+      </div>
+    </div>
+  );
+}
+
+const App = () => {
+  return (
+    <div className="App">
+      <Header />
+      <Body/>
+    </div>
+  );
+};
 
 export default App;
